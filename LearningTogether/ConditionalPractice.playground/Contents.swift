b913fc3c1a4451b81d2aebfda941b83b6375e024
Steph W <stephanie.worladge@rmit.edu.au>
2025-02-02 21:35:23 +1100

    Enumm update

--- LearningTogether/ConditionalPractice.playground/Contents.swift
+++ LearningTogether/ConditionalPractice.playground/Contents.swift
@@ -19,7 +19,8 @@ print(tNumber)
 // Make your if else statement more readable with case and switch
 let weather: String = "Sunny"
 
-// The value of weather is checked against each case
+// The value of weather is checked against each case . Works with    Strings, numbers, booleans, and enums. Decision making based on a constant or variable value
+
 switch weather {
 case "Sunny":
     print("Today is a sunny day, don't forget your sunscreen 🧴!")
@@ -30,5 +31,32 @@ case "Snow":
 default:
     print("Unknown weather condition.")
 }
+
+// enum - This creates a new type called Weather, and it can only have one of the predefined values: .rainy, .sunny, or .snowy. Defines a custom type with specific values, When you need a fixed set of possible values. Useful for Type safety and preventing invalid values.
+
+
+enum Weather {
+    case sunny
+    case rainy
+    case snowy
+}
+
+var todayWeather: Weather = .sunny
+
+switch todayWeather {
+case .sunny:
+    print("Today is a sunny day, don't forget your sunscreen 🧴!")
+case .rainy:
+    print("Today is a rainy day, bring an ☔️. ")
+case .snowy:
+    print("It's a cold one today, bundle up! ❄️")
+}
+
+///Final Takeaways
+
+///Use switch when checking a value against multiple cases.
+///Use enum to define a fixed set of values that belong together.
+///Combining switch with enum makes code safer and more readable.
+
 
 
